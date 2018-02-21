9f4fe2d5b9713105e95ee5f1357b12e731bae469
sylwiabr <s.brodacka@gmail.com>
2018-02-21 11:59:51 +0100

    use appdata instead of userProfile/appdata/roaming

--- luna-manager/src/Luna/Manager/Command/Install.hs
+++ luna-manager/src/Luna/Manager/Command/Install.hs
@@ -207,8 +207,8 @@ makeShortcuts packageBinPath appName = when (currentHost == Windows) $ do
     Logger.info "Creating Menu Start shortcut."
     bin         <- liftIO $ System.getSymbolicLinkTarget $ encodeString packageBinPath
     binAbsPath  <- Shelly.canonicalize $ (parent packageBinPath) </> (decodeString bin)
-    userProfile <- liftIO $ Environment.getEnv "userprofile"
-    let menuPrograms = (decodeString userProfile) </> "AppData" </> "Roaming" </> "Microsoft" </> "Windows" </> "Start Menu" </> "Programs" </> convert ((mkSystemPkgName appName) <> ".lnk")
+    appData <- liftIO $ Environment.getEnv "appdata"
+    let menuPrograms = (decodeString appData) </> "Microsoft" </> "Windows" </> "Start Menu" </> "Programs" </> convert ((mkSystemPkgName appName) <> ".lnk")
     exitCode <- liftIO $ Process.runProcess $ Process.shell  ("powershell" <> " \"$s=New-Object -ComObject WScript.Shell; $sc=$s.createShortcut(" <> "\'" <> (encodeString menuPrograms) <> "\'" <> ");$sc.TargetPath=" <> "\'" <> (encodeString binAbsPath) <> "\'" <> ";$sc.Save()\"")
     unless (exitCode == ExitSuccess) $ Logger.warning $ "Menu Start shortcut was not created. Powershell could not be found in the $PATH"
 
