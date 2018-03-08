f896c4c98857e899829f1eacc26acb16dfa66aa9
Sylwia Brodacka <sylwiabrodacka@MacBook-Pro-Sylwia.local>
2018-03-08 17:34:52 +0100

    code review

--- luna-manager/src/Luna/Manager/Command/Install.hs
+++ luna-manager/src/Luna/Manager/Command/Install.hs
@@ -174,9 +174,9 @@ checkIfAppAlreadyInstalledInCurrentVersion installPath appType pkgVersion = do
             then liftIO $ exitSuccess
             else checkIfAppAlreadyInstalledInCurrentVersion installPath appType pkgVersion
 
-data SolvePackagePathException = SolvePackagePathException Text deriving (Show)
-instance Exception SolvePackagePathException where
-    displayException (SolvePackagePathException file) = "Couldn't download file: " <> convert file <> " invalid URI address"
+data ResolvePackagePathException = ResolvePackagePathException Text deriving (Show)
+instance Exception ResolvePackagePathException where
+    displayException (ResolvePackagePathException file) = "Couldn't download file: " <> convert file <> " invalid URI address"
 
 downloadIfUri :: MonadInstall m => URIPath -> m FilePath
 downloadIfUri path = do
@@ -184,7 +184,7 @@ downloadIfUri path = do
     if doesFileExist then return $ fromText path
         else if URI.isURI $ convert path
             then downloadWithProgressBar path
-            else throwM $ SolvePackagePathException path
+            else throwM $ ResolvePackagePathException path
 
 downloadAndUnpackApp :: MonadInstall m => URIPath -> FilePath -> Text -> AppType -> Version -> m ()
 downloadAndUnpackApp pkgPath installPath appName appType pkgVersion = do
